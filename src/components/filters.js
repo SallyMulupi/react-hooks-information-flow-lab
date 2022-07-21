b2079997ed935b2627d2ab87d056f914393016c5
SallyMulupi <mulupisally2gmail.com>
2022-07-22 00:47:09 +0300

    add select name

--- src/components/filters.js
+++ src/components/filters.js
@@ -2,3 +2,4 @@ import React from 'react'
 const Filter = ({ onCategoryChange }) => {
     return (
         <div className="Filter">
+              <select name="filter" onChange={onCategoryChange}></select>
