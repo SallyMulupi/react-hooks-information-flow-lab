52a625d6427593c33a162860f86a15d272de10c8
SallyMulupi <mulupisally2gmail.com>
2022-07-22 00:55:39 +0300

    add filter oncategorychange

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,3 +4,10 @@ const Header = ({ onDarkModeClick, isDarkMode }) => {
         <header>
         <h2>Shopster</h2>
         <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+    </button>
+  </header>
+  )
+}
+
+export default Header;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -16,17 +16,7 @@ function ShoppingList({ items }) {
 
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+     <Filter onCategoryChange={handleCategoryChange} />
         ))}
       </ul>
     </div>
