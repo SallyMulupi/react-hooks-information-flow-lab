907c2a636e700589bf8a76169953540b177de54e
SallyMulupi <mulupisally2gmail.com>
2022-07-22 00:48:53 +0300

    add option value dessert

--- src/components/filters.js
+++ src/components/filters.js
@@ -6,3 +6,4 @@ const Filter = ({ onCategoryChange }) => {
               <option value="All">Filter by category</option>
               <option value="Produce">Produce</option>
               <option value="Dairy">Dairy</option>
+              <option value="Dessert">Dessert</option>
