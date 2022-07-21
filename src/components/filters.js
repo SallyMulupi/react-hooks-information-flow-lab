1ee68133bbecd8170f9f27db8d4f9cb5a445993d
SallyMulupi <mulupisally2gmail.com>
2022-07-22 00:47:40 +0300

    add option value all

--- src/components/filters.js
+++ src/components/filters.js
@@ -3,3 +3,4 @@ const Filter = ({ onCategoryChange }) => {
     return (
         <div className="Filter">
               <select name="filter" onChange={onCategoryChange}></select>
+              <option value="All">Filter by category</option>
